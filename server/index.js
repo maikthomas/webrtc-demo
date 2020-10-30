207c780b1cea5fa2c91a61befe7a3dad8ab57fda
Michael Thomas <MichaelGeorge.Thomas@vonage.com>
2020-10-30 16:43:04 +0100

    Add server

---
+++ server/index.js
@@ -0,0 +1,54 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+let client1;
+let client2;
+
+const getOtherSessionId = (id) => {
+  return id === client1.id ? client2.id : client1.id;
+}
+
+io.on('connection', (socket) => {
+  console.log('a user connected:', socket.id);
+  if (!client1) {
+    client1 = socket;
+    setTimeout(() => socket.emit('makeCallWithOffer'), 2000);
+  } else if (!client2) {
+    client2 = socket;
+    setTimeout(() => socket.emit('makeCallWithoutOffer'), 2000);
+  } else {
+    return;
+  }
+
+  socket.on('video-offer', (message) => {
+    console.log(message);
+    io.to(getOtherSessionId(socket.id)).emit('video-offer', message)
+  })
+  socket.on('video-answer', (message) => {
+    console.log(message);
+    io.to(getOtherSessionId(socket.id)).emit('video-answer', message)
+  })
+  socket.on('new-ice-candidate', (message) => {
+    console.log(message);
+    io.to(getOtherSessionId(socket.id)).emit('new-ice-candidate', message)
+  })
+
+  socket.on('disconnect', () => {
+    if (client1 && socket.id === client1.id) {
+      client1 = undefined;
+    } else if (client2 && socket.id === client2.id) {
+      client2 = undefined;
+    }
+  });
+
+  // socket.on('')
+});
+
+http.listen(8000, () => {
+  console.log('listening on *:8000');
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
+import io from 'socket.io-client';
+
+const socket = io("ws://localhost:8000", {
+  reconnectionDelayMax: 10000,
+  query: {
+    auth: "123"
+  }
+});
+
+
 const constraints = {
   'video': true,
   'audio': true
 }
+
+
 navigator.mediaDevices.getUserMedia(constraints)
   .then(stream => {
       console.log('Got MediaStream:', stream);
@@ -9,3 +21,77 @@ navigator.mediaDevices.getUserMedia(constraints)
   .catch(error => {
       console.error('Error accessing media devices.', error);
   });
+
+
+  const makeCall = (withOffer) => {
+    const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+    const peerConnection = new RTCPeerConnection(configuration);
+
+    peerConnection.addEventListener('icecandidate', event => {
+      console.log(event);
+    });
+
+    peerConnection.addEventListener('icecandidate', event => {
+      console.log('sending ice candidate', event.candidate);
+      if (event.candidate) {
+        socket.emit('new-ice-candidate', event.candidate);
+      }
+    });
+    socket.on('video-answer', (answer) => {
+      const remoteDesc = new RTCSessionDescription(answer);
+      peerConnection.setRemoteDescription(remoteDesc).then(() => {
+        console.log('set remote');
+      })
+    });
+
+    socket.on('video-offer', (offer) => {
+      console.log('received offer');
+      peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+      console.log('set remote');
+      peerConnection.createAnswer().then(answer => {
+        if (!withOffer) {
+          peerConnection.setLocalDescription(answer).then(() => {
+            console.log('set local');
+            socket.emit('video-answer', answer);
+          });
+        }
+      })
+
+    });
+
+    if (withOffer) {
+      peerConnection.createOffer().then(offer => {
+      peerConnection.setLocalDescription(offer).then(() => {
+        socket.emit('video-offer', offer);
+        console.log('set local');
+      });
+      });
+    }
+
+
+
+    socket.on('new-ice-candidate', candidate => {
+      if (candidate) {
+        peerConnection.addIceCandidate(candidate).catch((e) => {
+          console.error('Error adding received ice candidate', e);
+        })
+      }
+    });
+
+    peerConnection.addEventListener('connectionstatechange', () => {
+      if (peerConnection.connectionState === 'connected') {
+        console.log('peers connected');
+      }
+  });
+
+  }
+
+
+  socket.on('makeCallWithOffer', () => {
+    console.log('client1');
+    makeCall(true)
+  });
+  socket.on('makeCallWithoutOffer', () => {
+    console.log('client2');
+    makeCall(false);
+  })
