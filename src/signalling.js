94ab25d28c8fc74b930bc781470ac2a48493dc8c
Michael Thomas <MichaelGeorge.Thomas@vonage.com>
2020-11-06 12:40:32 +0100

    Try https for socketio

--- src/signalling.js
+++ src/signalling.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 const host = document.location.href.split('://')[1].split('/')[0];
 // const host = "localhost:8000";
 export const openSocket = () => {
-  const socket = io(`ws://${host}`, {
+  const socket = io(`https://${host}`, {
     path: '/signalling',
     reconnectionDelayMax: 10000,
     secure: true,
