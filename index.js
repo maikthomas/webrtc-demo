9d95bdb6e28db5af1e1fcf971a476795d12c30af
Michael Thomas <MichaelGeorge.Thomas@vonage.com>
2020-11-06 12:17:08 +0100

    Use port env var for server

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
-var http = require("http").createServer(app);
-var io = require("socket.io")(http, {
-  path: "/signalling",
-  origins: "*:*",
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+  path: '/signalling',
+  origins: '*:*',
 });
 
-app.use("/public", express.static(path.join(__dirname, "dist/public")));
+app.use('/public', express.static(path.join(__dirname, 'dist/public')));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/dist/index.html');
 });
 
 const clients = {
@@ -33,34 +33,34 @@ const getOtherClientSocket = (id) => {
 
 const getOtherSessionId = (id) => {
   const otherId = getOtherClientSocket(id)?.id;
-  console.log("otherId", otherId);
+  console.log('otherId', otherId);
   return otherId;
 };
 
 const sendClientsReadyEvent = () => {
-  console.log("sending clientsReady");
-  io.to(clients.client1.socket.id).emit("clientsReady", "client1");
-  io.to(clients.client2.socket.id).emit("clientsReady", "client2");
+  console.log('sending clientsReady');
+  io.to(clients.client1.socket.id).emit('clientsReady', 'client1');
+  io.to(clients.client2.socket.id).emit('clientsReady', 'client2');
 };
 
-io.on("connection", (socket) => {
-  let clientId = "";
-  console.log("a user connected:", socket.id);
+io.on('connection', (socket) => {
+  let clientId = '';
+  console.log('a user connected:', socket.id);
   if (!clients.client1.socket) {
-    console.log("client 1 connected");
-    clientId = "client1";
+    console.log('client 1 connected');
+    clientId = 'client1';
     clients[clientId].socket = socket;
   } else if (!clients.client2.socket) {
-    console.log("client 2 connected");
-    clientId = "client2";
+    console.log('client 2 connected');
+    clientId = 'client2';
     clients[clientId].socket = socket;
     // setTimeout(() => sendClientsReadyEvent(), 3000);
   } else {
     return;
   }
 
-  socket.on("client-ready", () => {
-    console.log("received client ready");
+  socket.on('client-ready', () => {
+    console.log('received client ready');
     clients[clientId].ready = true;
     if (
       clients.client1.ready &&
@@ -71,20 +71,20 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("video-offer", (message) => {
-    console.log("received video offer");
-    io.to(getOtherSessionId(socket.id)).emit("video-offer", message);
+  socket.on('video-offer', (message) => {
+    console.log('received video offer');
+    io.to(getOtherSessionId(socket.id)).emit('video-offer', message);
   });
-  socket.on("video-answer", (message) => {
+  socket.on('video-answer', (message) => {
     console.log(message);
-    io.to(getOtherSessionId(socket.id)).emit("video-answer", message);
+    io.to(getOtherSessionId(socket.id)).emit('video-answer', message);
   });
-  socket.on("new-ice-candidate", (message) => {
+  socket.on('new-ice-candidate', (message) => {
     console.log(message);
-    io.to(getOtherSessionId(socket.id)).emit("new-ice-candidate", message);
+    io.to(getOtherSessionId(socket.id)).emit('new-ice-candidate', message);
   });
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     clients[clientId] = {
       socket: null,
       ready: false,
@@ -92,6 +92,7 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(8000, () => {
-  console.log("listening on *:8000");
+const port = process.env.PORT || 8000
+http.listen(, () => {
+  console.log(`listening on *:${port}`);
 });
