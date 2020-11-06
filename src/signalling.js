22cc318c17c6e5743ea5692b8bd7e8adb209c96c
Michael Thomas <MichaelGeorge.Thomas@vonage.com>
2020-11-06 12:27:42 +0100

    Fix host

--- src/signalling.js
+++ src/signalling.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const host = document.location.href.split('://')[1].split('/');
+const host = document.location.href.split('://')[1].split('/')[0];
 // const host = "localhost:8000";
 export const openSocket = () => {
   const socket = io(`ws://${host}`, {
